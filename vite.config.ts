6ba3c35bf4909ec73c17793d182710f59da1f2e7
Martijn van Iersel <mvaniersel@gmail.com>
2025-07-19 20:01:12 -0600

    Prepare PWA version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
+/* eslint-disable camelcase */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
 	test: {
 		environment: 'jsdom'
-	}
+	},
+	base: './', // Use relative paths in index.html, makes our app relocatable.
+	plugins: [
+		vue(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Dr. Higgins',
+				short_name: 'higgins',
+
+				// URL relative to manifest file
+				start_url: './',
+
+				display: 'standalone',
+				background_color: '#ffffbb',
+				theme_color: '#7744ff',
+				icons: [ {
+					// relative to manifest file
+					src: './logo192.png',
+					sizes: '192x192',
+					type: 'image/png'
+				}, {
+					// relative to manifest file
+					src: './logo512.png',
+					sizes: '512x512',
+					type: 'image/png'
+				} ]
+			}
+		})
+	]
 });
