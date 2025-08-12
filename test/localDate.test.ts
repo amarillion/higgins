c0f280d2d43afc36b4cff9f437e0be74eb4a6d8d
Martijn van Iersel <mvaniersel@gmail.com>
2025-08-12 09:25:14 +0200

    Minor tweaks

--- test/localDate.test.ts
+++ test/localDate.test.ts
@@ -3,7 +3,7 @@ import { localDateToString } from '../src/util/localDate';
 
 describe('localDateStr', () => {
 
-	it('Environment should be configured with a explicit time zone', () => {
+	it('Environment should be configured with an explicit time zone', () => {
 		// since Date objects work with local time,
 		// to make tests indpendent of the test environment,
 		// you must set the time zone explicitly.
@@ -12,12 +12,12 @@ describe('localDateStr', () => {
 
 	it('Should return local date, not UTC date', () => {
 
-		const localDate = new Date('2024-10-01T22:00:00'); // Example date in Mexico time zone
+		const localDate = new Date('2024-10-01T22:00:00'); // in local time zone (Mexico)
 
 		// in UTC, this timestamp is a day ahead.
 		expect(localDate.toISOString().split('T')[0]).toBe('2024-10-02');
 
-		// the expected local date isn't though.
+		// the local date isn't though.
 		expect(localDateToString(localDate)).toBe('2024-10-01');
 	});
 
